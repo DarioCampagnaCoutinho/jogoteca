64f30bb45fefdc939c2f17e91e75f1f8e600f8c9
Dario Campagna Coutinho <campagnacoutinho67@gmail.com>
2020-12-26 15:05:27 -0300

    Criando a classe Jogo.

--- jogoteca.py
+++ jogoteca.py
@@ -3,9 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Super Mário', 'Angry Birds', 'Mortal Kombat', 'Free Fire']
+    jogo1 = Jogo('Super Mário', 'Ação', 'Super Nitendo')
+    jogo2 = Jogo('Pokemon Go', 'Ação', 'Android')
+    lista = [jogo1, jogo2]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
